5ede1eed8cc53d953a0683b657204285d17624f2
blastcepat777 <cucibaju988@gmail.com>
2026-03-28 12:59:53 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode');
+const TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+
+bot.onText(/\/start/, async (msg) => {
+    const chatId = msg.chat.id;
+
+    const client = new Client({
+        authStrategy: new LocalAuth({ clientId: String(chatId) })
+    });
+
+    client.on('qr', async (qr) => {
+        const qrImage = await qrcode.toBuffer(qr);
+        bot.sendPhoto(chatId, qrImage, { caption: 'Scan QR WhatsApp' });
+    });
+
+    client.on('ready', () => {
+        bot.sendMessage(chatId, '✅ WhatsApp Connected!');
+    });
+
+    client.initialize();
+});
